431c28285ae0354f8ce55c39500614aee77ecda3
Jananias minnu <jananias323@gmail.com>
2025-07-18 16:29:04 +0400

    ft_putstr

---
+++ ft_putstr.c
@@ -0,0 +1,9 @@
+#include "libft.h"
+
+void ft_putstr(char *s)
+{
+	while(*s)
+		write(1, s++, 1);
+}
+
+
---
+++ libft.h
@@ -0,0 +1,8 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <unistd.h>
+
+void ft_putstr(char *s);
+
+#endif
